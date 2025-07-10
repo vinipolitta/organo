385829f40adc7acc2001ada135326f8ba26d2d31
vinipolitta <vinicius.politta1@gmail.com>
2025-07-10 19:07:37 -0300

    feat: A importância do Angular como framework robusto para desenvolvimento de grandes aplicações.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+
+const components = [
+  CabecalhoComponent,
+  RodapeComponent
+]
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ...components
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
